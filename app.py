63a72dddcd9d10b745c860d91fbdc131c20ab1b4
alireza bahremand <abahrema@asu.edu>
2024-01-16 08:29:00 -0700

    switch assitant bc secret not working

--- app.py
+++ app.py
@@ -11,8 +11,7 @@ from tools import TOOL_MAP
 
 api_key = os.environ.get("OPENAI_API_KEY")
 client = openai.OpenAI(api_key=api_key)
-assistant_id_env = os.environ.get("ASSISTANT_ID")
-assistant_id = assistant_id_env
+assistant_id = "asst_EFGgVsqi0N185JROtg2VGKWC"
 assistant_title = os.environ.get("ASSISTANT_TITLE", "Sparky DSL Assistant API UI")
 instructions=""
 
