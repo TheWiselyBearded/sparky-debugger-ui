2eb0e1b2e2800d7228969a152693fcb7e2605174
Alireza Bahremand <abahrema@asu.edu>
2024-01-25 21:52:55 -0700

    refined prompt

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ from tools import TOOL_MAP
 api_key = os.environ.get("OPENAI_API_KEY")
 client = openai.OpenAI(api_key=api_key)
 assistant_id = "asst_FZVUka1PIysQNT0JRHg6pxWc"
-assistant_title = os.environ.get("ASSISTANT_TITLE", "Sparky DSL Assistant GPT4 UI")
+assistant_title = os.environ.get("ASSISTANT_TITLE", "Sparky DSL Assistant GPT UI")
 instructions=""
 
 def create_thread(content, file):
